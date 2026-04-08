d821ee3e1deba86ad0b4a2d2c6e9aeb68b21553d
Arpit Kumar <arpitkumar09ps@gmail.com>
2026-04-08 22:57:55 +0530

    feat: add imagekit rate limiting middleware to control upload requests

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createResume, deleteResume, getResumeById, updateResume, getPublicResumeById } from '../controllers/resumeController.js';
 import { protect } from '../middlewares/authMiddleware.js';
+import { imagekitRateLimit } from '../middlewares/imagekitRateLimit.js';
 import upload from '../configs/multer.js';
 
 const resumeRouter = express.Router();
 
 resumeRouter.post('/create',protect, createResume);
-resumeRouter.put('/update',upload.single('image'),protect, updateResume);
+resumeRouter.put('/update', protect, imagekitRateLimit, upload.single('image'), updateResume);
 resumeRouter.delete('/delete/:resumeId',protect, deleteResume);
 resumeRouter.get('/get/:resumeId',protect, getResumeById);
 resumeRouter.get('/public/:resumeId', getPublicResumeById);
